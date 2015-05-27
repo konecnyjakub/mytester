a1a2c8b4669b325e027833df86da2296de2a07f9
Jakub Konečný <jakub.konecny2@centrum.cz>
2015-05-27 13:51:16 +0200

    simplified tests runner

--- src/run_tests.php
+++ src/run_tests.php
@@ -1,11 +1,10 @@
 <?php
 namespace MyTester;
-define("TESTS_DIR", dirname(__FILE__) . "..");
 
-require_once "../src/bootstrap.php";
+require_once "./bootstrap.php";
 Environment::setup();
 
 foreach(glob("../tests/*.phpt") as $filename) {
-  require TESTS_DIR . "/$filename";
+  require $filename;
 }
 ?>
