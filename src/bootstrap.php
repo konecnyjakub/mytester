9c242e5e42e45eba8d1bfea67af7a62b54a99b55
Jakub Konečný <jakub.konecny2@centrum.cz>
2015-05-27 13:46:31 +0200

    simplified bootstrap

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,9 +1,7 @@
 <?php
-define("APP_DIR", dirname(__FILE__));
-
-require APP_DIR . "/Environment.php";
-require APP_DIR . "/Assert.php";
-require APP_DIR . "/Job.php";
-require APP_DIR . "/Runner.php";
-require APP_DIR . "/TestCase.php";
+require "./Environment.php";
+require "./Assert.php";
+require "./Job.php";
+require "./Runner.php";
+require "./TestCase.php";
 ?>
