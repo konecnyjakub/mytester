e1f3da2e7c51fcfecff64a88831110bcdfeaab8b
Jakub Konečný <jakub.konecny2@centrum.cz>
2015-05-27 03:35:55 +0200

    got rid of property TestCase::$runner, use local variable instead

--- src/TestCase.php
+++ src/TestCase.php
@@ -8,20 +8,18 @@ namespace MyTester;
  */
 abstract class TestCase {
   const METHOD_PATTERN = '#^test[A-Z0-9_]#';
-  /** @var \MyTester\Runner */
-  private $runner;
   
   function run() {
     $className = get_class($this);
-    $this->runner = new Runner($className);
+    $runner = new Runner($className);
     $r = new \ReflectionObject($this);
     $methods = array_values(preg_grep(self::METHOD_PATTERN, array_map(function(\ReflectionMethod $rm) {
       return $rm->getName();
     }, $r->getMethods())));
     foreach($methods as $method) {
-      $this->runner->addJob($className . "::$method", array($this, $method));
+      $runner->addJob($className . "::$method", array($this, $method));
     }
-    $output = $this->runner->run();
+    $output = $runner->run();
     foreach($output as $line) {
       echo "$line";
     }
