0511a8f589378a1efd54c900392daf3f9ff54acb
Jakub Konečný <jakub.konecny2@centrum.cz>
2015-05-27 21:28:19 +0200

    renamed constant APP_DIR to TESTER_APP_DIR (to avoid clashes with other apps)

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,9 +1,9 @@
 <?php
-define("APP_DIR", dirname(__FILE__));
+define("TESTER_APP_DIR", dirname(__FILE__));
 
-require APP_DIR . "/Environment.php";
-require APP_DIR . "/Assert.php";
-require APP_DIR . "/Job.php";
-require APP_DIR . "/Runner.php";
-require APP_DIR . "/TestCase.php";
+require TESTER_APP_DIR . "/Environment.php";
+require TESTER_APP_DIR . "/Assert.php";
+require TESTER_APP_DIR . "/Job.php";
+require TESTER_APP_DIR . "/Runner.php";
+require TESTER_APP_DIR . "/TestCase.php";
 ?>
