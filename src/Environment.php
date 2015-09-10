ef323ebc4422a3bee139eadbd9fdf96d7e7b4146
Jakub Konečný <jakub.konecny2@centrum.cz>
2015-09-10 11:42:55 +0200

    explicitly allow assert() in Environment::setup()

--- src/Environment.php
+++ src/Environment.php
@@ -117,6 +117,7 @@ abstract class Environment {
    */
   static function setup($output = "screen") {
     if(!self::$set) {
+      assert_options(ASSERT_ACTIVE, 1);
       assert_options(ASSERT_QUIET_EVAL, 1);
       assert_options(ASSERT_WARNING, 0);
       //assert_options(ASSERT_CALLBACK, array(__CLASS__, "assertionFail"));
