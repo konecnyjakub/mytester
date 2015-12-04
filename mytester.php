2448577a122c24677ec4f93f2c64c219eca79f67
Jakub Konečný <jakub.konecny2@centrum.cz>
2015-12-04 18:43:07 +0100

    shortened mytester.php

--- mytester.php
+++ mytester.php
@@ -1,12 +1,8 @@
 <?php
 require __DIR__ . "/vendor/autoload.php";
 require __DIR__ . "/src/bootstrap.php";
+MyTester\Environment::setup();
 
-$folder = __DIR__ . "/tests";
-$output = "screen";
-
-MyTester\Environment::setup($output);
-
-$tester = new MyTester\Tester($folder);
+$tester = new MyTester\Tester(__DIR__ . "/tests");
 $tester->execute();
 ?>
