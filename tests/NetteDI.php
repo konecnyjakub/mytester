ebe123fe17bfe4b6ff82c86465b5c5aca950c57b
Jakub Konečný <jakub.konecny2@centrum.cz>
2017-12-09 18:42:41 +0100

    updated dependencies

--- tests/NetteDI.php
+++ tests/NetteDI.php
@@ -9,6 +9,8 @@ Nette\Utils\FileSystem::createDir(__DIR__ . "/temp");
 $configurator->setTempDirectory(__DIR__ . "/temp");
 $configurator->addConfig(__DIR__ . "/config.neon");
 $container = $configurator->createContainer();
-$result = $container->getService("mytester.runner")->execute();
+/** @var \MyTester\Bridges\NetteDI\TestsRunner $runner */
+$runner = $container->getService("mytester.runner");
+$result = $runner->execute();
 exit((int) $result);
 ?>
