0ea9f2dc7956444c9577f99480b16eeac218912b
Jakub Konečný <jakub.konecny2@centrum.cz>
2020-09-28 17:29:49 +0200

    marked ICodeCoverageEngine as internal

--- src/ICodeCoverageEngine.php
+++ src/ICodeCoverageEngine.php
@@ -3,6 +3,10 @@ declare(strict_types=1);
 
 namespace MyTester;
 
+/**
+ * @author Jakub Konečný
+ * @internal
+ */
 interface ICodeCoverageEngine {
   public function getName(): string;
   public function isAvailable(): bool;
