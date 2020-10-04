31002e3a67c62ca6eb39f5def2564135ec54bb09
Jakub Konečný <jakub.konecny2@centrum.cz>
2020-10-04 16:40:40 +0200

    fixed code-coverage.php for codesniffer on PHP 8

--- code-coverage.php
+++ code-coverage.php
@@ -1,16 +1,21 @@
 <?php
 declare(strict_types=1);
 
+use SebastianBergmann\CodeCoverage\CodeCoverage;
+use SebastianBergmann\CodeCoverage\Driver\Selector;
+use SebastianBergmann\CodeCoverage\Filter;
+use SebastianBergmann\CodeCoverage\Report\Clover;
+
 require __DIR__ . "/vendor/autoload.php";
 
-$filter = new \SebastianBergmann\CodeCoverage\Filter();
+$filter = new Filter();
 $filter->includeDirectory(__DIR__ . "/src");
 
-$coverage = new \SebastianBergmann\CodeCoverage\CodeCoverage((new \SebastianBergmann\CodeCoverage\Driver\Selector())->forLineCoverage($filter), $filter);
+$coverage = new CodeCoverage((new Selector())->forLineCoverage($filter), $filter);
 $coverage->start("My Tester");
 register_shutdown_function(function() use ($coverage) {
   $coverage->stop();
-  (new \SebastianBergmann\CodeCoverage\Report\Clover())->process($coverage, __DIR__ . "/coverage.xml");
+  (new Clover())->process($coverage, __DIR__ . "/coverage.xml");
 });
 
 require __DIR__ . "/src/mytester.php";
