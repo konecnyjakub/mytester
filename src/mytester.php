cfe0020b7cd376ad66b96c3ef4599727a7955443
Jakub Konečný <jakub.konecny2@centrum.cz>
2021-12-30 19:19:06 +0100

    call findVendorDirectory() only one time in mytester.php

--- src/mytester.php
+++ src/mytester.php
@@ -4,14 +4,16 @@ declare(strict_types=1);
 
 require_once __DIR__ . "/functions.php";
 
-require findVendorDirectory() . "/autoload.php";
+$vendorDirectory = findVendorDirectory();
+
+require $vendorDirectory . "/autoload.php";
 
 use MyTester\Tester;
 use Nette\CommandLine\Parser;
 
 $cmd = new Parser("", [
     "path" => [
-        Parser::VALUE => dirname(findVendorDirectory()) . "/tests",
+        Parser::VALUE => $vendorDirectory . "/tests",
     ],
     "--colors" => [
         Parser::OPTIONAL => true,
