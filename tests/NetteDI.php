8b7605f062f94a2d933e79a8507c4b88fdd96a17
Jakub Konečný <jakub.konecny2@centrum.cz>
2021-12-30 13:48:57 +0100

    updated dependencies

--- tests/NetteDI.php
+++ tests/NetteDI.php
@@ -7,7 +7,6 @@ require __DIR__ . "/../vendor/autoload.php";
 require __DIR__ . "/../src/functions.php";
 
 use MyTester\Bridges\NetteDI\ContainerSuiteFactory;
-use MyTester\Tester;
 use Nette\Configurator;
 
 $configurator = new Configurator();
@@ -15,7 +14,7 @@ Nette\Utils\FileSystem::createDir(__DIR__ . "/temp");
 $configurator->setTempDirectory(__DIR__ . "/temp");
 $configurator->addConfig(__DIR__ . "/config.neon");
 $container = $configurator->createContainer();
-/** @var Tester $runner */
-$runner = $container->getByType(Tester::class);
+/** @var MyTester\Tester $runner */
+$runner = $container->getByType(MyTester\Tester::class);
 assert($runner->testSuiteFactory instanceof ContainerSuiteFactory, "Test suite factory is not a " . ContainerSuiteFactory::class . " instance.");
 $runner->execute();
