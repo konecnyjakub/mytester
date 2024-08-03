d9e949c4313b2c7a3d88e775ca085e81d7b1ab9e
Jakub Konečný <jakub.konecny2@centrum.cz>
2024-08-03 14:46:06 +0200

    ci: use our own code coverage collector

--- code-coverage.php
+++ code-coverage.php
@@ -1,23 +1,6 @@
 <?php
-
 declare(strict_types=1);
 
-use SebastianBergmann\CodeCoverage\CodeCoverage;
-use SebastianBergmann\CodeCoverage\Driver\Selector;
-use SebastianBergmann\CodeCoverage\Filter;
-use SebastianBergmann\CodeCoverage\Report\Cobertura;
-use SebastianBergmann\FileIterator\Facade;
-
 require __DIR__ . "/vendor/autoload.php";
-
-$filter = new Filter();
-$filter->includeFiles((new Facade())->getFilesAsArray(__DIR__ . "/src", ".php"));
-
-$coverage = new CodeCoverage((new Selector())->forLineCoverage($filter), $filter);
-$coverage->start("My Tester");
-register_shutdown_function(function () use ($coverage) {
-    $coverage->stop();
-    (new Cobertura())->process($coverage, __DIR__ . "/coverage.xml");
-});
 
 require __DIR__ . "/tests/NetteDI.php";
