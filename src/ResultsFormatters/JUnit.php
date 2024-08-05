8a1129cae71a0b1a2783acf714f1911802e9f60a
Jakub Konečný <jakub.konecny2@centrum.cz>
2024-08-05 20:12:10 +0200

    added JUnit results formatter (#19)

---
+++ src/ResultsFormatters/JUnit.php
@@ -0,0 +1,161 @@
+<?php
+declare(strict_types=1);
+
+namespace MyTester\ResultsFormatters;
+
+use DOMDocument;
+use MyTester\IResultsFormatter;
+use MyTester\Job;
+use MyTester\JobResult;
+use MyTester\TestCase;
+use ReflectionClass;
+use ReflectionFunction;
+use ReflectionFunctionAbstract;
+use ReflectionMethod;
+
+/**
+ * JUnit results formatter for Tester
+ * Outputs the results into a file in JUnit format
+ * @see https://www.ibm.com/docs/en/developer-for-zos/16.0?topic=formats-junit-xml-format
+ * @see https://llg.cubic.org/docs/junit/
+ *
+ * @author Jakub Konečný
+ * @internal
+ */
+final class JUnit implements IResultsFormatter
+{
+    private string $baseFileName = "junit.xml";
+
+    /** @var TestCase[] */
+    private array $testCases = [];
+
+    public function setup(): void
+    {
+    }
+
+    public function setTestsFolder(string $folder): void
+    {
+    }
+
+    public function reportTestCase(TestCase $testCase): void
+    {
+        $this->testCases[] = $testCase;
+    }
+
+    public function render(int $totalTime): string
+    {
+        if (!extension_loaded("dom")) {
+            return "";
+        }
+
+        $totalTests = 0;
+        $totalFailures = 0;
+        $totalSkipped = 0;
+        $totalWarnings = 0;
+        $totalAssertions = 0;
+
+        $document = new DOMDocument("1.0", "UTF-8");
+        $document->formatOutput = true;
+
+        $testSuites = $document->createElement("testsuites");
+        $testSuites->setAttribute("name", "Project test suite by My Tester");
+        $testSuites->setAttribute("time", (string) round($totalTime / 1000, 6));
+
+        foreach ($this->testCases as $testCase) {
+            $testSuiteTests = count($testCase->jobs);
+            $totalTests += $testSuiteTests;
+            $testSuiteFailures = count(array_filter($testCase->jobs, function (Job $job) {
+                return $job->result === JobResult::FAILED;
+            }));
+            $totalFailures += $testSuiteFailures;
+            $testSuiteSkipped = count(array_filter($testCase->jobs, function (Job $job) {
+                return $job->result === JobResult::SKIPPED;
+            }));
+            $totalSkipped += $testSuiteSkipped;
+            $testSuiteWarnings = count(array_filter($testCase->jobs, function (Job $job) {
+                return $job->result === JobResult::WARNING;
+            }));
+            $totalWarnings += $testSuiteWarnings;
+            $rc = new ReflectionClass($testCase::class);
+            $testSuiteTime = 0;
+            $testSuiteAssertions = 0;
+
+            $testSuite = $document->createElement("testsuite");
+            $testSuite->setAttribute("id", $rc->getName());
+            $testSuite->setAttribute("name", $testCase->getSuiteName());
+            $testSuite->setAttribute("file", (string) $rc->getFileName());
+            $testSuite->setAttribute("tests", (string) $testSuiteTests);
+            $testSuite->setAttribute("failures", (string) $testSuiteFailures);
+            $testSuite->setAttribute("skipped", (string) $testSuiteSkipped);
+            $testSuite->setAttribute("warnings", (string) $testSuiteWarnings);
+
+            foreach ($testCase->jobs as $job) {
+                $testSuiteTime += $job->totalTime;
+                $testSuiteAssertions += $job->totalAssertions;
+                $totalAssertions += $job->totalAssertions;
+                $reflectionCallback = $this->createReflectionFromCallback($job->callback);
+
+                $testCaseElement = $document->createElement("testcase");
+                $testCaseElement->setAttribute("name", $job->name);
+                $testCaseElement->setAttribute("class", $testCase::class);
+                $testCaseElement->setAttribute("classname", (string) str_replace("\\", ".", $testCase::class));
+                $testCaseElement->setAttribute("file", (string) $rc->getFileName());
+                $testCaseElement->setAttribute("line", (string) $reflectionCallback->getStartLine());
+                $testCaseElement->setAttribute("time", (string) round($job->totalTime / 1000, 6));
+                $testCaseElement->setAttribute("assertions", (string) $job->totalAssertions);
+
+                switch ($job->result) {
+                    case JobResult::SKIPPED:
+                        $skipped = $document->createElement("skipped");
+                        if (is_string($job->skip)) {
+                            $skipped->setAttribute("message", $job->skip);
+                        }
+                        $testCaseElement->appendChild($skipped);
+                        break;
+                    case JobResult::FAILED:
+                        $message = $job->output . "\n\n" .
+                            (string) $rc->getFileName() . ":" . $reflectionCallback->getStartLine();
+                        $failure = $document->createElement("failure", $message);
+                        $failure->setAttribute("type", "assert");
+                        // each assertion failure is printed at a new line but \n is malformed as value of attribute
+                        $failure->setAttribute("message", (string) str_replace("\n", " ", $job->output));
+                        $testCaseElement->appendChild($failure);
+                        break;
+                }
+
+                $testSuite->appendChild($testCaseElement);
+            }
+
+            $testSuite->setAttribute("time", (string) round($testSuiteTime / 1000, 6));
+            $testSuite->setAttribute("assertions", (string) $testSuiteAssertions);
+            $testSuites->appendChild($testSuite);
+        }
+
+        $testSuites->setAttribute("tests", (string) $totalTests);
+        $testSuites->setAttribute("failures", (string) $totalFailures);
+        $testSuites->setAttribute("skipped", (string) $totalSkipped);
+        $testSuites->setAttribute("warnings", (string) $totalWarnings);
+        $testSuites->setAttribute("assertions", (string) $totalAssertions);
+        $document->appendChild($testSuites);
+
+        return (string) $document->saveXML();
+    }
+
+    public function getOutputFileName(string $folder): string
+    {
+        return "$folder/{$this->baseFileName}";
+    }
+
+    private function createReflectionFromCallback(callable $callback): ReflectionFunctionAbstract
+    {
+        if (is_array($callback)) {
+            return new ReflectionMethod($callback[0], $callback[1]);
+        }
+        /** @var string $callback */
+        if (str_contains($callback, "::")) {
+            return new ReflectionMethod(...explode("::", $callback));
+        } else {
+            return new ReflectionFunction($callback);
+        }
+    }
+}
---
+++ tests/ResultsFormatters/JUnitTest.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+
+namespace MyTester\ResultsFormatters;
+
+use MyTester\Attributes\TestSuite;
+use MyTester\TestCase;
+
+/**
+ * Test suite for class JUnit
+ *
+ * @author Jakub Konečný
+ */
+#[TestSuite("Results formatter JUnit")]
+final class JUnitTest extends TestCase
+{
+    public function testRender(): void
+    {
+        $outputFormatter = new JUnit();
+        $testCase1 = new TestCaseOne();
+        $testCase1->run();
+        $outputFormatter->reportTestCase($testCase1);
+        $testCase2 = new TestCaseTwo();
+        $testCase2->run();
+        $outputFormatter->reportTestCase($testCase2);
+        $testCase3 = new TestCaseThree();
+        $testCase3->run();
+        $outputFormatter->reportTestCase($testCase3);
+        $result = $outputFormatter->render(1);
+        $this->assertSame(file_get_contents(__DIR__ . "/junit_output.xml"), $result);
+    }
+
+    public function testGetOutputFileName(): void
+    {
+        $outputFormatter = new JUnit();
+        $this->assertSame("/var/project/junit.xml", $outputFormatter->getOutputFileName("/var/project"));
+        $this->assertSame("/var/project/sub1/junit.xml", $outputFormatter->getOutputFileName("/var/project/sub1"));
+    }
+}
