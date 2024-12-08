82ca2abef5747ee8730cfe20fde34b87d36f49e3
Jakub Konečný <jakub.konecny2@centrum.cz>
2024-12-08 19:12:13 +0100

    do not use Nette\CommandLine\Console in ConsoleColors

--- src/ConsoleColors.php
+++ src/ConsoleColors.php
@@ -3,41 +3,41 @@ declare(strict_types=1);
 
 namespace MyTester;
 
-use Nette\CommandLine\Console;
-
 /**
  * Helper for colorizing output in console
  *
  * @author Jakub Konečný
- * @property bool $useColors
  */
 final class ConsoleColors
 {
     use \Nette\SmartObject;
 
-    private readonly Console $console;
+    private const array COLORS = [
+        "black" => "0;30",
+        "maroon" => "0;31",
+        "green" => "0;32",
+        "olive" => "0;33",
+        "navy" => "0;34",
+        "magenta" => "0;35",
+        "teal" => "0;36",
+        "silver" => "0;37",
+        "gray" => "1;30",
+        "red" => "1;31",
+        "lime" => "1;32",
+        "yellow" => "1;33",
+        "blue" => "1;34",
+        "fuchsia" => "1;35",
+        "aqua" => "1;36",
+        "white" => "1;37",
+    ];
 
-    private bool $useColors = false;
-
-    public function __construct()
-    {
-        $this->console = new Console();
-        $this->setUseColors(false);
-    }
+    public bool $useColors = false;
 
     public function color(string $text, ?string $color = null): string
     {
-        return $this->console->color($color, $text);
-    }
-
-    protected function isUseColors(): bool
-    {
-        return $this->useColors;
-    }
-
-    protected function setUseColors(bool $useColors): void
-    {
-        $this->useColors = $useColors;
-        $this->console->useColors($useColors);
+        if (!$this->useColors || $color === null || !array_key_exists($color, static::COLORS)) {
+            return $text;
+        }
+        return "\033[" . static::COLORS[$color] . "m" . $text . "\033[0m";
     }
 }
