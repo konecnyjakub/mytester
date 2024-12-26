06c1a4384386b8785f90150cc5a29a8e7161aa62
Jakub Konečný <jakub.konecny2@centrum.cz>
2024-12-26 19:25:02 +0100

    added a missing documentation comment

--- src/Attributes/IgnoreDeprecations.php
+++ src/Attributes/IgnoreDeprecations.php
@@ -9,6 +9,8 @@ use Attribute;
  * Ignore deprecations attribute
  *
  * Causes deprecations to not be reported for a test
+ *
+ * @author Jakub Konečný
  */
 #[Attribute(Attribute::TARGET_METHOD)]
 final class IgnoreDeprecations
