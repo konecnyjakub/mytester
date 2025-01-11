103582bd546727c3fb38de88e51a4af7cd3ff263
Jakub Konečný <jakub.konecny2@centrum.cz>
2025-01-11 13:04:32 +0100

    broke down method AnnotationsDataProvider::getData into smaller ones

--- src/AnnotationsDataProvider.php
+++ src/AnnotationsDataProvider.php
@@ -35,64 +35,108 @@ final readonly class AnnotationsDataProvider implements IDataProvider
             return [];
         }
 
-        if ($this->annotationsReader->hasAnnotation(self::ANNOTATION_SIMPLE_NAME, $class, $method)) {
-            /** @var array<string|int, mixed>[] $result */
-            $result = $this->annotationsReader->getAnnotationMulti(self::ANNOTATION_SIMPLE_NAME, $class, $method);
-            return $result;
+        $dataSets = $this->getSimpleDataSets($reflection);
+        if ($dataSets !== []) {
+            return $dataSets;
         }
 
+        $dataSets = $this->getNormalDataSets($reflection, $class);
+        if ($dataSets !== []) {
+            return $dataSets;
+        }
+
+        $dataSets = $this->getExternalDataSets($reflection);
+        if ($dataSets !== []) {
+            return $dataSets;
+        }
+
+        return [];
+    }
+
+    /**
+     * @return iterable<string|int, mixed>[] Values for consecutive calls of the the method
+     */
+    private function getSimpleDataSets(ReflectionMethod $reflection): iterable
+    {
+        $class = $reflection->class;
+        $method = $reflection->name;
+        if (!$this->annotationsReader->hasAnnotation(self::ANNOTATION_SIMPLE_NAME, $class, $method)) {
+            return [];
+        }
+        /** @var array<string|int, mixed>[] $result */
+        $result = $this->annotationsReader->getAnnotationMulti(self::ANNOTATION_SIMPLE_NAME, $class, $method);
+        return $result;
+    }
+
+    /**
+     * @return iterable<string|int, mixed>[] Values for consecutive calls of the the method
+     * @throws InvalidDataProviderException
+     */
+    private function getNormalDataSets(ReflectionMethod $reflection, object $object): iterable
+    {
+        $class = $reflection->class;
+        $method = $reflection->name;
         $dataProvider = $this->annotationsReader->getAnnotation(self::ANNOTATION_NAME, $class, $method);
-        if (is_string($dataProvider)) {
-            $className = $reflection->getDeclaringClass()->getName();
-            try {
-                $reflection = new ReflectionMethod($class, $dataProvider);
-                if (!$reflection->isPublic()) {
-                    throw new InvalidDataProviderException("Method $className::$dataProvider is not public.");
-                }
-                /** @var callable $callback */
-                $callback = [$class, $dataProvider];
-                $result = call_user_func($callback);
-                if (!is_iterable($result)) {
-                    throw new InvalidDataProviderException(
-                        "Method $className::$dataProvider has to return an array or an iterable object."
-                    );
-                }
-                /** @var iterable<string|int, mixed>[] $result */
-                return $result;
-            } catch (\ReflectionException $e) {
-                throw new InvalidDataProviderException("Method $className::$dataProvider does not exist.", 0, $e);
-            }
+        if (!is_string($dataProvider)) {
+            return [];
         }
+        $className = $reflection->getDeclaringClass()->getName();
+        try {
+            $reflection = new ReflectionMethod($class, $dataProvider);
+            if (!$reflection->isPublic()) {
+                throw new InvalidDataProviderException("Method $className::$dataProvider is not public.");
+            }
+            /** @var callable $callback */
+            $callback = [$object, $dataProvider];
+            $result = call_user_func($callback);
+            if (!is_iterable($result)) {
+                throw new InvalidDataProviderException(
+                    "Method $className::$dataProvider has to return an array or an iterable object."
+                );
+            }
+            /** @var iterable<string|int, mixed>[] $result */
+            return $result;
+        } catch (\ReflectionException $e) {
+            throw new InvalidDataProviderException("Method $className::$dataProvider does not exist.", 0, $e);
+        }
+    }
 
+    /**
+     * @return iterable<string|int, mixed>[] Values for consecutive calls of the the method
+     * @throws InvalidDataProviderException
+     */
+    private function getExternalDataSets(ReflectionMethod $reflection): iterable
+    {
+        $class = $reflection->class;
+        $method = $reflection->name;
         $dataProviderExternal = $this->annotationsReader->getAnnotation(
             self::ANNOTATION_EXTERNAL_NAME,
             $class,
             $method
         );
-        if (is_string($dataProviderExternal)) {
-            try {
-                [$className, $methodName] = explode("::", $dataProviderExternal);
-                $reflection = new ReflectionMethod($className, $methodName);
-                if (!$reflection->isPublic()) {
-                    throw new InvalidDataProviderException("Method $dataProviderExternal is not public.");
-                }
-                if (!$reflection->isStatic()) {
-                    throw new InvalidDataProviderException("Method $dataProviderExternal is not static.");
-                }
-                /** @var string&callable $dataProviderExternal */
-                $result = call_user_func($dataProviderExternal);
-                if (!is_iterable($result)) {
-                    throw new InvalidDataProviderException(
-                        "Method $dataProviderExternal has to return an array or an iterable object."
-                    );
-                }
-                /** @var iterable<string|int, mixed>[] $result */
-                return $result;
-            } catch (\ReflectionException $e) {
-                throw new InvalidDataProviderException("Method $dataProviderExternal does not exist.", 0, $e);
-            }
+        if (!is_string($dataProviderExternal)) {
+            return [];
         }
-
-        return [];
+        try {
+            [$className, $methodName] = explode("::", $dataProviderExternal);
+            $reflection = new ReflectionMethod($className, $methodName);
+            if (!$reflection->isPublic()) {
+                throw new InvalidDataProviderException("Method $dataProviderExternal is not public.");
+            }
+            if (!$reflection->isStatic()) {
+                throw new InvalidDataProviderException("Method $dataProviderExternal is not static.");
+            }
+            /** @var string&callable $dataProviderExternal */
+            $result = call_user_func($dataProviderExternal);
+            if (!is_iterable($result)) {
+                throw new InvalidDataProviderException(
+                    "Method $dataProviderExternal has to return an array or an iterable object."
+                );
+            }
+            /** @var iterable<string|int, mixed>[] $result */
+            return $result;
+        } catch (\ReflectionException $e) {
+            throw new InvalidDataProviderException("Method $dataProviderExternal does not exist.", 0, $e);
+        }
     }
 }
