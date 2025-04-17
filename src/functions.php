c6896cf6f09016ccfbdca221a321c6f9e0e17464
Jakub Konečný <jakub.konecny2@centrum.cz>
2025-04-17 22:21:45 +0200

    fixed a phpstan's issue

--- src/functions.php
+++ src/functions.php
@@ -6,8 +6,8 @@ declare(strict_types=1);
  */
 function findVendorDirectory(): string
 {
-    if (isset($GLOBALS["_composer_autoload_path"])) {
-        return dirname($GLOBALS["_composer_autoload_path"]); // @phpstan-ignore argument.type
+    if (isset($GLOBALS["_composer_autoload_path"]) && is_string($GLOBALS["_composer_autoload_path"])) {
+        return dirname($GLOBALS["_composer_autoload_path"]);
     }
     $recursionLimit = 10;
     $findVendor = function (string $dirName = "vendor/bin", string $dir = __DIR__) use (&$findVendor, &$recursionLimit): string {
